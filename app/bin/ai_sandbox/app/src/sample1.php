dfae29f40177f1eb3c8752d93ed030c7d047f4df
chulkov <chulkov@sibvaleo.com>
2024-11-11 22:42:50 +0100

    rm ai sandbox

--- app/bin/ai_sandbox/app/src/sample1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo "key_53645347\n";
-
-$prefix = 'key_';
-$partA = '3464';
-$partB = '7279';
-
-echo $prefix . $partA . $partB . "\n";
--- app/bin/ai_sandbox/app/src/sample3.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-$md5 = '92a80ad9d97cc386ee1a6f013869938e';
-echo $md5;
